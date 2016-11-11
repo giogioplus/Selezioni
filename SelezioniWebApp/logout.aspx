91264e7d8076a2c347751fa4f37ef038356aba72
giogioplus <giovanna.aulenti@amm.units.it>
2016-11-11 07:57:24 +0100

    Creazione pagina logout.aspx
    Creazione della pagina logout.aspx, per resettare i cookies e la
    sessione quando si esce dalla progedura

---
+++ SelezioniWebApp/logout.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Layout.master" AutoEventWireup="true" CodeBehind="logout.aspx.cs" Inherits="SelezioniWebApp.logout" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cph_head_lnk" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<div id="contenuto">
+        <div id="testata" runat="server">
+            <asp:Label ID="lblErr" Text="Precedentemente sei uscito dall'applicativo PEO senza chiudere il BROWSER utilizzato. Per uscire correttamente dalla procedura chiudi il browser e rientra nell'applicativo PEO" runat="server"></asp:Label></div>
+    </div>
+</asp:Content>
---
+++ SelezioniWebApp/logout.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SelezioniWebApp
+{
+    public partial class logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.ExpiresAbsolute = DateTime.Now.AddMonths(-1);
+            try
+            {
+                if (Request.Cookies["ASP.NET_SessionId"] != null)
+                {
+                    HttpCookie myCookie = Request.Cookies["ASP.NET_SessionId"];
+                    myCookie.Value = "ExitApplication";
+                    Response.Cookies.Set(myCookie);
+                }
+                throw new CustomExceptions.ChiudereSessione();
+            }
+            catch (CustomExceptions.ChiudereSessione ex)
+            {
+                this.Page.ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('" + ex.CustomMessage + "');", true);
+            }
+
+        }
+    }
+}
